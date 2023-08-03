2fe192de050f4ebb1297a0b7b7478d09d3285e2c
venkatraman <kmlvenkat@gmail.com>
2023-08-03 12:08:25 +0530

    fjfhdj

--- task_five/src/Plugin/Field/FieldFormatter/FieldFormatter.php
+++ task_five/src/Plugin/Field/FieldFormatter/FieldFormatter.php
@@ -48,6 +48,7 @@ class FieldFormatter extends FormatterBase {
     $summary[] = $this->t("concatenate with : @concat", ["@concat" => $this->getSetting('concat')]);
     return $summary;
   }
+
   /**
    * {@inheritdoc}
    */
@@ -64,4 +65,5 @@ class FieldFormatter extends FormatterBase {
 
     return $elements;
   }
+
 }
